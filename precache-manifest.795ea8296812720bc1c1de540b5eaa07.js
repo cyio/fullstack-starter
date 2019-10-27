19c66069775c81c722ad760b3911ec64c0875fff
cyio <icaner@qq.com>
2019-10-27 11:58:47 +0800

    update

---
+++ precache-manifest.795ea8296812720bc1c1de540b5eaa07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2074a26ec888f553ba",
+    "url": "/css/app.ad0a62f2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7291bb2266e63820ea1639aac70c9b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a370754389c4efac4ed0",
+    "url": "/js/about.b34cf0be.js"
+  },
+  {
+    "revision": "ab2074a26ec888f553ba",
+    "url": "/js/app.2a7d30e5.js"
+  },
+  {
+    "revision": "fc0c6249d7c8f368f99b",
+    "url": "/js/chunk-vendors.54eb080e.js"
+  },
+  {
+    "revision": "e75ced6e1a2dc6d932b729ef49842e94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
